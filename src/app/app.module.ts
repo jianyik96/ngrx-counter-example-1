0a62ea32ff3ebe92238a379f4193b3312d0b9992
JY <jianyi96kang@gmail.com>
2023-05-22 16:35:09 +0800

    initial commit

---
+++ src/app/app.module.ts
@@ -0,0 +1,23 @@
+import { NgModule, isDevMode } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
+import { StoreModule } from '@ngrx/store';
+import { MyCounterComponent } from './my-counter/my-counter.component';
+import { counterReducer } from './counter.reducer';
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    MyCounterComponent
+  ],
+  imports: [
+    BrowserModule,
+    AppRoutingModule,
+    StoreModule.forRoot({ count: counterReducer}),
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
